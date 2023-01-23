82e88a6bf2837fa9d114ba1e17d30c61fe612ac7
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-01-22 22:42:59 -0500

    Feat: implement character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function() {
+
+  $( "#tweet-text" ).on( "input", function( event ) {
+  
+    // Extract the counter element from the DOM
+    const counter = $(this).next().children()[1]
+
+    // Calculate the remaining characters
+    counter.value = 140 - this.value.length;
+    
+    // Determine if the counter is negative to help stylize accordingly
+    if (counter.value < 0){
+      counter.setAttribute("negative", "true");
+    } else {
+      counter.setAttribute("negative", "false");
+    }
+    
+});
+
+});
